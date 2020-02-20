857b81c6a9474602145efeda39c5dbf3ce7f7567
rricha91 <60485491+rricha91@users.noreply.github.com>
2020-02-20 17:18:27 -0500

    Update ToyDriver.java

--- ToyDriver.java
+++ ToyDriver.java
@@ -2,13 +2,32 @@ import javax.swing.JOptionPane;
 public class ToyDriver {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null,"Welcome to CMSC 03");
-		String name = JOptionPane.showInputDialog("Enter Your Name: ");
+		Toy toy = new Toy();
+		LoopPrompt loop = new LoopPrompt();
 		
-		JOptionPane.showMessageDialog(null, "You entered " + name);
+		System.out.print("Assignment 2: Birthday Toys");
 		
-		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
-		JOptionPane.showMessageDialog(null, "Hi " + "! You are " + age + " years old.");
+		JOptionPane.showMessageDialog(null,"Howdy, welcome to the Toys R' Us Birthday Brigade!\nThe first emergancy delivery service for birthday boys and girls!");
+		
+		do {
+			String name = JOptionPane.showInputDialog("Who's the lucky kid you're addressing this to?");
+		
+			
+			JOptionPane.showMessageDialog(null, "We`ve addressed the order to \"" + name + "\".");
+
+			
+			toy.setAge(Integer.parseInt(JOptionPane.showInputDialog("How old will " + name + " be turning?")));
+		
+			
+			JOptionPane.showMessageDialog(null, toy.getAge() + "?!\nGosh, they grow up so fast.");
+			
+			toy.setToy(JOptionPane.showInputDialog("Alright, so what's the name of the toy you're getting them?\n(Remember to only use lowercase letters!)"));
+			
+			JOptionPane.showMessageDialog(null, "Ordering \"" + toy.getToy() + "\"...");
+		} while(!loop.ageP(toy));
+		
+		
+		
 	}
 
 }
